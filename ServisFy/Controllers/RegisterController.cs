6070ce2b130be8668ef6fc3c5c3cca841b1ca65b
aysckmk <aysua15@gmail.com>
2022-08-19 13:17:38 +0300

    m

---
+++ ServisFy/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServisFy.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
